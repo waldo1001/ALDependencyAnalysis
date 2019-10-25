e18911504aec96ccb02f5983224e54cd7b7c13d8
Eric Wauters <eric.wauters@dynex.be>
2019-10-25 16:38:04 +0200

    Startscripts for circular dependency checks

---
+++ PSScripts/Circular Dependencies/Recursive Dependencies_Process.ps1
@@ -0,0 +1,36 @@
+﻿function Get-Recursive() {
+    param(
+        [string] $MainModule,
+        [int] $CurrentLevel,
+        [string] $FullLink,
+        $ModuleToProcess,
+        $AlreadyProcessed
+    )
+    foreach ($Sub in $ModuleToProcess) {
+        if ($CurrentLevel -gt 30) {
+            write-host "levels too damn high, aborting [$FullLink]"
+            continue
+        }
+        if ($AlreadyProcessed -contains $Sub) {
+            continue
+        }
+        $AlreadyProcessed += $Sub
+        if ($Sub -eq $MainModule) {
+            Write-Host "$($FullLink)$($Sub) [$CurrentLevel deep]"
+            $Global:Total += 1
+            continue
+        }
+
+        $SubModule = $ModuleLink[$Sub]
+        Get-Recursive -MainModule $MainModule -CurrentLevel ($CurrentLevel + 1) -FullLink ($FullLink + $Sub + ' > ') -ModuleToProcess $Submodule -AlreadyProcessed $AlreadyProcessed
+
+    }
+}
+
+$Global:Total = 0
+
+foreach ($Module in $ModuleLink.GetEnumerator()) {
+    Get-Recursive -MainModule $Module.key -CurrentLevel 1 -FullLink ($Module.Name + ' > ') -ModuleToProcess $Module.Value -AlreadyProcessed @()
+}
+
+Write-Host "$($Global:Total) deep links detected"
---
+++ PSScripts/Circular Dependencies/Recursive Dependencies_Process2.ps1
@@ -0,0 +1,38 @@
+﻿function Get-Recursive() {
+    param(
+        [string] $MainModule,
+        [int] $CurrentLevel,
+        [string] $FullLink,
+        $ModuleToProcess,
+        [ref]$AlreadyProcessed
+    )
+    foreach ($Sub in $ModuleToProcess) {
+        if ($CurrentLevel -gt 30) {
+            write-host "levels too damn high, aborting [$FullLink]"
+            continue
+        }
+        if ($AlreadyProcessed.value -contains $Sub) {
+            continue
+        }        
+        if ($Sub -eq $MainModule) {
+            Write-Host "$($FullLink)$($Sub) [$CurrentLevel deep]"
+            $Global:Total += 1
+            continue
+        }
+
+        $AlreadyProcessed.value += $Sub
+
+        $SubModule = $ModuleLink[$Sub]
+        Get-Recursive -MainModule $MainModule -CurrentLevel ($CurrentLevel + 1) -FullLink ($FullLink + $Sub + ' > ') -ModuleToProcess $Submodule -AlreadyProcessed $AlreadyProcessed
+
+    }
+}
+
+$Global:Total = 0
+
+foreach ($Module in $ModuleLink.GetEnumerator()) {
+    $already = @()
+    Get-Recursive -MainModule $Module.key -CurrentLevel 1 -FullLink ($Module.Name + ' > ') -ModuleToProcess $Module.Value -AlreadyProcessed ([ref]$already)
+}
+
+Write-Host "$($Global:Total) deep links detected"
